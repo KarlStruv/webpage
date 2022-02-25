10eb0aa7a568b57fef75885b695fe62818cf11cf
Kārlis Strūve <struve3@gmail.com>
2022-02-25 04:41:42 +0200

    Removed navbar migration, seeder and adjusted AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,13 +27,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
-
-        View::composer('*', function($view)
-
-        {
-            $navbars = Navbar::orderBy('ordering')->get();
-            $view->with('navbars', $navbars);
-        });
     }
 }
